837f7ace433535b169a5e1eb0d039746e029fca7
Alexey Kopievskiy <alexeykopievskiy@gmail.com>
2020-04-09 14:06:14 +0300

    replace tpls

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/", express.static(__dirname + '/src'));
 
 app.use(function (req, res, next) {
-  if (req.path === '/v-fall') app.set('views', './src/views/v-fall');
-  else if (req.path === '/ladines') app.set('views', './src/views/ladines');
-  else if (req.path === '/showlaxy') app.set('views', './src/views/showlaxy');
+  if (req.path.startsWith('/v-fall')) app.set('views', './src/views/v-fall');
+  else if (req.path.startsWith('/ladines')) app.set('views', './src/views/ladines');
+  else if (req.path.startsWith('/showlaxy')) app.set('views', './src/views/showlaxy');
   else res.send('lolz')
   next()
 })
@@ -25,7 +25,7 @@ app.get('/v-fall', function (req, res, next) {
     next()
 });
 
-app.get('/error', function (req, res, next) {
+app.get('/v-fall/error', function (req, res, next) {
   res.render('error');
   next()
 });
@@ -54,8 +54,54 @@ app.get('/ladines', function (req, res) {
   res.render('index');
 });
 
+app.get('/ladines/error', function (req, res, next) {
+  res.render('error');
+  next()
+});
+
+app.get('/ladines/news', function (req, res) {
+  res.render('news');
+});
+
+app.get('/ladines/list', function (req, res) {
+  res.render('list');
+});
+
+app.get('/ladines/contacts', function (req, res) {
+  res.render('contacts');
+});
+
+app.get('/ladines/article', function (req, res) {
+  res.render('article');
+});
+
 app.get('/showlaxy', function (req, res) {
   res.render('index');
+});
+
+app.get('/showlaxy/error', function (req, res, next) {
+  res.render('error');
+  next()
+});
+
+app.get('/showlaxy/news', function (req, res) {
+  res.render('news');
+});
+
+app.get('/showlaxy/list', function (req, res) {
+  res.render('list');
+});
+
+app.get('/showlaxy/inner', function (req, res) {
+  res.render('inner');
+});
+
+app.get('/showlaxy/contacts', function (req, res) {
+  res.render('contacts');
+});
+
+app.get('/showlaxy/article', function (req, res) {
+  res.render('article');
 });
 
 app.listen(3000, function () {
